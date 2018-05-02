f268cd5686866e2174cd52626d510df449572949
macho <machopman_shop@hotmail.com>
2018-05-02 22:48:07 +0700

    sadabj

--- picture.py
+++ picture.py
@@ -1,16 +1,21 @@
 from flask import Flask
-from PIL import Image
+import tkinter
+from PIL import Image, ImageTk
+from io import BytesIO
 import requests
+from pandas._libs import window
+
 app = Flask(__name__)
+
 @app.route('/image/<string:event>', methods=['GET'])
 def pic(event):
 
     Movie_URL= 'http://mandm.plearnjai.com/API/detailMovie.php?idmovie='+event
     r = requests.get(url=Movie_URL)
     data = r.json()
-    url = data['response'][0]['detailMovie'][0]['Poster']
-    image = Image.open(requests.get(url, stream=True).raw)
-    return image
+    r = data['response'][0]['detailMovie'][0]['Poster']
+    return r
+
 
 if __name__ == '__main__':
     app.run()
